ad6c5d06c87c9dd697a793861b7ac15f157c9aad
Ahmed Bentajer <a.bentajer@gmail.com>
2024-03-07 13:41:02 +0100

    Query Selector all examples

--- sandbox.js
+++ sandbox.js
@@ -6,7 +6,14 @@ console.log(para);
 
 // query multiple elements at once
 const paras = document.querySelectorAll('p');
+//Iterate on all selectors returned by ocument.querySelectorAll('p');
+paras.forEach(para => {
+    console.log('---> : ',para);
+})
+
 const errors = document.querySelectorAll('.error');
 
 console.log(paras, errors);
+
+//Use table index to show elements in paras array
 console.log(paras[1], errors[0]);
